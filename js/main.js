dc9ddd430dcb837c50438e1b505503e08adf4737
LESANF <nagong1000@naver.com>
2021-09-16 23:44:38 +0900

    docs: add header main-menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
+const badgeEl = document.querySelector("header .badges");
 // document.querySelector(".search input")
-/* 위와같이 연달아 쓸 수 있지만 분할하여 쓰는게 좋다. */
+/* 위와같이 연달아 쓸 수 있다. */
 
 const fcsHandler = () => searchInputEl.focus();
 
@@ -15,6 +16,12 @@ const inputHandler = () => {
   searchInputEl.setAttribute("placeholder", "통합검색");
 };
 
+const scrollHandler = () => {
+  console.log("scr");
+};
+
 searchEl.addEventListener("click", fcsHandler);
 searchInputEl.addEventListener("focus", inputHandler);
 searchInputEl.addEventListener("blur", blurHandler); /** blur == !focus */
+
+window.addEventListener("scroll", scrollHandler);

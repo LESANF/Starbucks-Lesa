2c205b8351f58f79f7e50b039ffccce0774529bb
LESANF <nagong1000@naver.com>
2021-09-24 06:22:24 +0900

    docs: add notice and promotion Swiper

--- js/main.js
+++ js/main.js
@@ -45,8 +45,45 @@ const fadeInImg = (ele, idx) => gsap.to(ele, 1, { delay: (idx + 1) * 0.7, opacit
 fadeEls.forEach((ele, idx) => fadeInImg(ele, idx));
 
 // new Swiper(선택자, 옵션)
-new Swiper(".notice-line .swiper", {
-  direction: "vertical",
-  autoplay: true,
-  loop: true,
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical", // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
 });
+
+new Swiper(".promotion .swiper-container", {
+  //direction: "horizontal" - Swiper의 기본값은 수평
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 버튼
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+
+const toggleClickHandler = () => {
+  isHidePromotion = !isHidePromotion;
+
+  if (isHidePromotion) {
+    //숨김 처리
+    promotionEl.classList.add("hide");
+  } else {
+    //보임 처리
+    promotionEl.classList.remove("hide");
+  }
+};
+
+promotionToggleBtn.addEventListener("click", toggleClickHandler);

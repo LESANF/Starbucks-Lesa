6b744b3716540de57ee30215b7cca3565fa55369
LESANF <nagong1000@naver.com>
2021-09-28 07:42:31 +0900

    docs: add Sections

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-const badgeEl = document.querySelector("header .badges");
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+const badgeEl = document.querySelector('header .badges');
 // document.querySelector(".search input")
 /* 위와같이 연달아 쓸 수 있다. */
-const fadeEls = document.querySelectorAll(".visual .fade-in");
+const fadeEls = document.querySelectorAll('.visual .fade-in');
 
 const fcsHandler = () => searchInputEl.focus();
 
 const blurHandler = () => {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
 };
 
 const inputHandler = () => {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 };
 
 const scrollHandler = () => {
@@ -22,22 +22,22 @@ const scrollHandler = () => {
     //배지 숨기기
     gsap.to(badgeEl, 0.6, {
       opacity: 0,
-      display: "none",
+      display: 'none',
     });
   } else {
     //배지 보이기
     gsap.to(badgeEl, 0.6, {
       opacity: 1,
-      display: "block",
+      display: 'block',
     });
   }
 };
 
-searchEl.addEventListener("click", fcsHandler);
-searchInputEl.addEventListener("focus", inputHandler);
-searchInputEl.addEventListener("blur", blurHandler); /** blur == !focus */
+searchEl.addEventListener('click', fcsHandler);
+searchInputEl.addEventListener('focus', inputHandler);
+searchInputEl.addEventListener('blur', blurHandler); /** blur == !focus */
 
-window.addEventListener("scroll", _.throttle(scrollHandler, 300)); /* ms, 300 = 3s */
+window.addEventListener('scroll', _.throttle(scrollHandler, 300)); /* ms, 300 = 3s */
 
 /* main Image 애니메이션 처리 */
 const fadeInImg = (ele, idx) => gsap.to(ele, 1, { delay: (idx + 1) * 0.7, opacity: 1 }); //0.7 -> 1.4 -> 2.1 -> 2.7 (ele = 4, idx = 0, 1, 2, 3)
@@ -45,13 +45,13 @@ const fadeInImg = (ele, idx) => gsap.to(ele, 1, { delay: (idx + 1) * 0.7, opacit
 fadeEls.forEach((ele, idx) => fadeInImg(ele, idx));
 
 // new Swiper(선택자, 옵션)
-const noticeSwiper = new Swiper(".notice-line .swiper-container", {
-  direction: "vertical", // 수직 슬라이드
+const noticeSwiper = new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
   loop: true, // 반복 재생 여부
 });
 
-const promotionSwiper = new Swiper(".promotion .swiper-container", {
+const promotionSwiper = new Swiper('.promotion .swiper-container', {
   //direction: "horizontal" - Swiper의 기본값은 수평
   slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10, //슬라이드 사이 여백
@@ -61,17 +61,17 @@ const promotionSwiper = new Swiper(".promotion .swiper-container", {
     delay: 5000,
   },
   pagination: {
-    el: ".promotion .swiper-pagination", //페이지 번호 요소 선택자
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
     clickable: true, // 사용자의 페이지 번호 요소 제어 버튼
   },
   navigation: {
-    prevEl: ".promotion .swiper-prev",
-    nextEl: ".promotion .swiper-next",
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next',
   },
 });
 
-const promotionEl = document.querySelector(".promotion");
-const promotionToggleBtn = document.querySelector(".toggle-promotion");
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
 
 const toggleClickHandler = () => {
@@ -79,14 +79,14 @@ const toggleClickHandler = () => {
 
   if (isHidePromotion) {
     //숨김 처리
-    promotionEl.classList.add("hide");
+    promotionEl.classList.add('hide');
   } else {
     //보임 처리
-    promotionEl.classList.remove("hide");
+    promotionEl.classList.remove('hide');
   }
 };
 
-promotionToggleBtn.addEventListener("click", toggleClickHandler);
+promotionToggleBtn.addEventListener('click', toggleClickHandler);
 
 //범위 랜덤 함수(소수점 2자리까지: x.xx)
 const random = (min, max) => parseFloat((Math.random() * (max - min) + min).toFixed(2));
@@ -101,6 +101,6 @@ const floatingObject = (selector, delay, size) => {
   });
 };
 
-floatingObject(".floating1", 1, 15);
-floatingObject(".floating2", 0.5, 15);
-floatingObject(".floating3", 1.5, 20);
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
